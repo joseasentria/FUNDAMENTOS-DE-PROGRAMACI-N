eca44e2e4d4483a59b212d52c2893286d579c29e
joseasentria <121901867+joseasentria@users.noreply.github.com>
2023-01-11 13:40:43 -0500

    Add files via upload

---
+++ Fundamentos de programación estructurada/2. Estructuras de control/a. Condicionales/Condicionales.java
@@ -0,0 +1,16 @@
+// Usando solo ifs
+if (temperatura > 25) {
+    System.out.println("A la playa!!!");
+}
+
+if (temperatura <= 25) {
+    System.out.println("Esperando al buen tiempo...");
+}
+
+// Usando if con else
+// Si la temperatura es mayor que 25 ... y si no ...
+if (temperatura > 25) {
+    System.out.println("A la playa!!!");
+} else {
+    System.out.println("Esperando al buen tiempo...");
+}
