c221535cda6c01d17262b8d8fdb880e90c20896b
joseasentria <121901867+joseasentria@users.noreply.github.com>
2023-01-11 13:10:00 -0500

    Add files via upload

---
+++ Fundamentos de programación estructurada/1. Variables/Variables.java
@@ -0,0 +1,9 @@
+class PrimeraVariable
+{
+public static void main ( String[] args ) { 
+   String mensaje = "Valor inicial";
+   System.out.println(mensaje);
+   mensaje = "Valor modificado" ;
+   System.out.println(mensaje);
+ }
+}
